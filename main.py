71f9a36b978c27bc6afa5ddbbeb0c211ec52a63f
Ashad <ashad001sp@gmail.com>
2023-04-15 03:01:31 +0500

    added stopping words

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import openai
 import os
-
-
-# openai.api_key = os.getenv('OPENAI_API_KEY')
+from textblob import TextBlob
+import re
 
 openai.api_key = os.environ["OPENAI_API_KEY"]
-# openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 messages = [
     {
@@ -15,17 +13,30 @@ messages = [
                     You can ask about the candidate's previous work experience, education, or anything else you think is relevant to the job. \
                     You can also ask the candidate about various programming questions such as code completion and bug fixes. \
                     User will answer the question and you have to verify whether it is correct or not!. \
-                    And also rate the answer's accuracy betwee 0 and 10 (10 being most suitable answer) and display in ['accuracy'] in the very end of each response. ",
+                    And also rate the answer's accuracy between 0 and 10 (10 being most suitable answer) and display in ['accuracy'] at the very end of each response. To stop the interview, please type 'stop'.",
     },
 ]
+
+
+
+stop_words = ["stop", "quit", "exit", "end", "finish"]
 
 while True:
     message = input("user: ")
     if message:
+        if any(word in message.lower() for word in stop_words):
+            print("Interview stopped.")
+            print("Please complete the interview before requesting a score.")
+            break
         messages.append(
             {"role": "user", "content": message},
         )
         chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
-    reply = chat.choices[0].message.content
-    print(f"Chatbot: , {reply}")
-    messages.append({"role": "system", "content": reply})
+        reply = chat.choices[0].message.content
+        print(f"Chatbot: {reply}")
+        messages.append({"role": "system", "content": reply})
+        blob = TextBlob(reply)
+        tone_score = blob.sentiment.polarity
+        accuracy = round((tone_score + 1) * 5)
+        print(f"Accuracy: {accuracy}")
+        
