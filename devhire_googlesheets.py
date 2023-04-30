3a862656939fc4766cf0e6e021b80218411bdc35
Aheed Tahir <52594408+Tahiralira@users.noreply.github.com>
2023-04-30 18:41:42 +0500

    Update devhire_googlesheets.py
    Added Gui Template for Email, Contributed by @abdulhaseeb-user 
    also a few minor code changes for accessibility.

--- devhire_googlesheets.py
+++ devhire_googlesheets.py
@@ -2,9 +2,12 @@ import gspread #for sheets
 import re #for string verification such as email and name formats
 import random
 from email.message import EmailMessage
-import smtplib
+import smtplib #for sending verification emails through python
 import time
 import ssl
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
 gc = gspread.service_account(filename='sheetAuth.json') #giving sheet access
 wks = gc.open("devhire_Database").sheet1
 
@@ -26,12 +29,55 @@ def send_otp_email(email,verification_code):
         lines = f.readlines()
         devhire_email = lines[0].strip()
         devhire_email_password = lines[1].strip()
-
-    mail_content = EmailMessage()
-    mail_content.set_content(f"Your Verification Code For DevHire Signup Is : {verification_code}")
+    mail_content = MIMEMultipart("alternative")
     mail_content['Subject'] = 'DevHire One Time Verification'
     mail_content['From'] = devhire_email
     mail_content['To'] = email
+    text = f"Your Verification Code For DevHire Signup Is : {verification_code}"
+    #HTML content
+    html = f"""
+    <!DOCTYPE html>
+    <html>
+    <body>
+        <!-- Logo section -->
+        <div id="logo">
+            <td style="background-color:white;">
+                <img src="https://lh3.googleusercontent.com/drive-viewer/AFGJ81rxuocJq92t5lIyKSE51q-xBEsMu3ah0tJxlnpw_VHkmzZ3NSo1yqWIrd0EI8W3QvSJIIRZgwWx_dEHjVuRkZ7rQYixxw=s2560" alt="logo.png" height="250" width=auto style="padding-top: 1rem;">
+            </td>
+        </div>
+
+        <!-- Main content -->
+        <div class="content" style="align: left 1px; background: -webkit-linear-gradient(0deg,#39b1b2 ,#000000 100%);">
+            <h1 style="font-size: 2.5em;">Verification Code</h1>
+            <p style="font-size: 1.5em;">Please use the verification code below to sign in.</p>
+            <br>
+            <table>
+                <tr>
+                    <td>
+                        <br>
+                        <h1 strong style="font-size:3.5em; color:#ffffff;letter-spacing: 0.2em;">{verification_code} </h3>
+                    </td>
+                </tr>
+            </table>
+            <p style="font-size:1.5em" ;>If you didn't request this, you can ignore this email.</p>
+            <br>
+            <p strong style="font-size:1.5em" ;>Thanks,</p>
+            <p style="font-size:1.5em" ;>The DevHire Team</p>
+            <br><br><br>
+            <h2>Contact Us</h2>
+            <p>For any questions or inquiries, please send us an email at <a
+                    href="mailto:devhire.info@gmail.com">info@devday.com</a>.</p>
+        </div>
+    </body>
+
+    </html>
+        
+    """
+    part1 = MIMEText(text ,'plain')
+    part2 = MIMEText(html,'html')
+    mail_content.attach(part1)
+    mail_content.attach(part2)
+    
     
     with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as server:
         server.login(devhire_email,devhire_email_password)
@@ -42,11 +88,16 @@ def verify_user():
         send_otp_email(email,verification_code)
             
         start_time = time.time()
-        while time.time() - start_time < 120:
+        while time.time() - start_time < 1:
             entered_otp = input("Enter the code sent to your email (within 2 minutes) : ")
-            if entered_otp != verification_code:
+            if entered_otp != verification_code or time.time() - start_time > 1:
                 print("Verification Code Invalid or Expired, try again and if code not recieved check your mail SPAM")
-                continue
+                break
+            if entered_otp == verification_code:
+                if time.time() - start_time > 1:
+                    print("Verification Code Expired")
+                    break
+                break
             else:
                 break
         
@@ -70,9 +121,12 @@ while True:
         score1 = wks.cell(row_num,4).value
         score2 = wks.cell(row_num,5).value
         score3 = wks.cell(row_num,6).value
-        verify_user()
-        print(f"Welcome back {first_name}, You have signed in successfully\nHere are your scores from your last session\nSCORE 1: {score1}\nSCORE 2: {score2}\nSCORE 3: {score3}")
-        break    
+        if verify_user():
+            print(f"Welcome back {first_name}, You have signed in successfully\nHere are your scores from your last session\nSCORE 1: {score1}\nSCORE 2: {score2}\nSCORE 3: {score3}")
+            break    
+        else:
+            while not verify_user():
+                verify_user()
 
 
     if choice == "n" or choice == "N":
@@ -92,7 +146,7 @@ while True:
             existing_emails = wks.col_values(3) #to check for a returning user and keeping new users unique
             #ashad this is the method you can use for retrieving data as 3rd coloumn is hardcoded for emails
             if email in existing_emails:
-                print("This Email is already registered. Please try again with a different email.")
+                print("This Email is already registered. Please try again with a different email or Sign in as a returning user.")
                 continue
             verify_user()
             break
